6454e9b035d178794e46d281cf55aa4810b68255
__.anle.__ <147271364+anle404@users.noreply.github.com>
2024-01-29 01:15:25 +0700

    Revert "Merge branch 'master' of https://github.com/AFanOfHololive/Chimken"
    This reverts commit fe28d918b2632fd778d59b9973cfcec84f80b59f, reversing
    changes made to 0b6034824b9030fdbaa739fa6fb4ce55021184e3.

--- chicken_dinner/buy.py
+++
@@ -1,9 +0,0 @@
-import functools
-from flask import (Blueprint, render_template, request, url_for)
-
-bp = Blueprint('buy', __name__, url_prefix='/buy')
-
-
-@bp.route('/')
-def trade():
-    return render_template('buy/buy.html', style="styles.css")
