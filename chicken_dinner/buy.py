119130d2c00980bb5fb0bbef1d34ea78795744fe
AFanOfHololive <ios2004@icloud.com>
2024-02-01 02:52:44 +0700

    fixed up the buy module, added table need css for it

--- chicken_dinner/buy.py
+++ chicken_dinner/buy.py
@@ -1,5 +1,4 @@
 from flask import (Blueprint, render_template)
-import plotly.graph_objs as go
 
 bp = Blueprint('buy', __name__, url_prefix='/buy')
 
@@ -14,37 +13,30 @@ item = {
 
 @bp.route('/<int:item_id>')
 def index(item_id):
-    # Sample data
-    x_data = [1, 2, 3, 4, 5]
-    y_data = [10, 15, 13, 17, 18]
-
+    item['id'] = item_id
     # Pass data to HTML template
-    return render_template('buy/buy.html', x_data=x_data, y_data=y_data, item=item)
+    return render_template('buy/buy.html', item=item)
 
-# def buy(item_id):
-#    # temp
-#    item['id'] = item_id
-#
-#    # Sample data
-#    x_data = [1, 2, 3, 4, 5]
-#    y_data = [10, 15, 13, 17, 18]
-#
-#    # Create Plotly figure
-#    fig = go.Figure()
-#    fig.add_trace(go.Scatter(x=x_data, y=y_data, mode='lines+markers', name='line'))
-#
-#    fig.update_layout(
-#        title='Pricing History',
-#        xaxis_title='Time',
-#        yaxis_title='Price',
-#        plot_bgcolor='rgba(0,0,0,0)',
-#        paper_bgcolor='rgba(0,0,0,0)',
-#        font=dict(color='white'),  # Set font color
-#        margin=dict(l=1, r=1, t=100, b=1),
-#        yaxis_showgrid=False,  # Make x-axis gridlines invisible
-#        showlegend=False,
-#
-#    )
-#
-#    plot_json = fig.to_json()
-#    return render_template('buy/buy.html', item=item, plot_json=plot_json)
+
+@bp.route('/get-listing-data', methods=['POST'])
+def get_listing_data():
+    html_response = ""
+
+    listing_data = []
+
+    lol = {
+        'unit_price': 100,
+        'quantity': 12,
+        'expiration': '29 years',
+        'from': 'me'
+    }
+
+    listing_data.append(lol)
+
+    html_response = ""
+    for listing in listing_data:
+        html_response += "<tr>"
+        for key, value in listing.items():
+            html_response += f"<td>{value}</td>"
+        html_response += "</tr>"
+        return html_response
