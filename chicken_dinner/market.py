a2956538e5a5aba5f4ba8fc3a6072ab2ea931234
AFanOfHololive <ios2004@icloud.com>
2024-02-03 17:22:20 +0700

    added htmx and redirects to market

--- chicken_dinner/market.py
+++ chicken_dinner/market.py
@@ -1,5 +1,11 @@
-import functools
-from flask import (Blueprint, render_template, request, url_for)
+from flask import (
+    Blueprint,
+    render_template,
+    request,
+    url_for,
+    redirect,
+    make_response
+)
 
 bp = Blueprint('market', __name__, url_prefix='/market')
 
@@ -7,3 +13,13 @@ bp = Blueprint('market', __name__, url_prefix='/market')
 @bp.route('/')
 def index():
     return render_template('market/market.html')
+
+
+@bp.route('/redirect')
+def buy():
+    url = url_for('buy.index', item_id=193)
+    response = make_response(
+        redirect(url, code=200)
+    )
+    response.headers['HX-Redirect'] = url
+    return response
