ec63ca29984537bb9c406cbe424970005be1ea5b
__.anle.__ <147271364+anle404@users.noreply.github.com>
2024-02-29 15:13:42 +0700

    Poosay

--- chicken_dinner/__init__.py
+++ chicken_dinner/__init__.py
@@ -1,4 +1,5 @@
 import os
+import mysql.connector
 
 from flask import Flask
 
@@ -9,7 +10,7 @@ def create_app(test_config=None):
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+    ) 
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
@@ -23,7 +24,13 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
     # INIT BLUEPRINTS AND DB HERE
+    mydb = mysql.connector.connect(
+        host="sql-lol.duckdns.org",
+        user="root",
+        password="lol"
+    )
 
     from . import buy
     app.register_blueprint(buy.bp)
