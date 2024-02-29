acedf8135669dcdd27d63707f84d8993d055b487
AFanOfHololive <ios2004@icloud.com>
2024-02-29 15:16:50 +0700

    solid snake

--- chicken_dinner/__init__.py
+++ chicken_dinner/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask
 
 
@@ -7,9 +6,9 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
+            SECRET_KEY='dev',
+            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+            )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
