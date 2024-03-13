aa5b4b6d1c0286e066e35293a74394ca5bdafb7d
AFanOfHololive <ios2004@icloud.com>
2024-03-13 19:48:19 +0700

    moved solids to deals tab in profile

--- chicken_dinner/profile.py
+++ chicken_dinner/profile.py
@@ -1,9 +1,24 @@
-import functools
-import random
 from flask import (Blueprint, render_template, request, url_for, g, make_response, redirect)
 from .auth import login_required
+import json
+from web3 import Web3, HTTPProvider
 
 bp = Blueprint('profile', __name__, url_prefix='/profile')
+
+
+# connect to local ethereum node
+web3 = Web3(HTTPProvider('http://127.0.0.1:9545'))
+web3.eth.default_account = web3.eth.accounts[0]
+
+# getting compiled contract
+compiled_contract_path = 'build/contracts/EthExchange.json'
+contract_address = '0x7eF5B70AFB8dB7138fb6082727C89fAD5C19eA7B'
+
+with open(compiled_contract_path) as file:
+    contract_json = json.load(file)
+    contract_abi = contract_json['abi']
+
+contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
 itemArray = []
 
@@ -14,31 +29,34 @@ def load_items(user_id):
     global itemArray
     print("fetching")
     g.cursor.execute("SELECT * FROM NFT_Item WHERE UserID = %s", (user_id,))
-    itemArray = g.cursor.fetchall()     
+    itemArray = g.cursor.fetchall()
     return itemArray
+
 
 def load_users(user_id):
     global userArray
     g.cursor.execute("SELECT * FROM Users WHERE UserID = %s", (user_id,))
-    userArray = g.cursor.fetchone()     
+    userArray = g.cursor.fetchone()
     return userArray
+
 
 def load_deal(user_id):
     g.cursor.execute("SELECT Deals.*, User_From.Name AS FromName, User_To.Name AS ToName FROM Deals INNER JOIN Users AS User_From ON Deals.FromUserID = User_From.UserID INNER JOIN Users AS User_To ON Deals.ToUserID = User_To.UserID WHERE ToUserID = %s", user_id)
     dealsArray = g.cursor.fetchall()
     return dealsArray
 
+
 def search_deal_item(idArray):
     if len(idArray) < 2:
         sql = f"SELECT * From NFT_Item WHERE ItemID = {idArray[0]}"
     else:
         sql = f"SELECT * FROM NFT_Item WHERE ItemID = {idArray[0]} OR ItemID = {idArray[1]}"
-        
+
     g.cursor.execute(sql)
     fromArray = g.cursor.fetchall()
 
     return fromArray
-    
+
 
 eth1 = {
         "plus": "+ 2.3ETH"
@@ -70,8 +88,8 @@ def index(user_id):
     if user_id == g.user["UserID"]:
         is_own_profile = True
 
-    itemArray = load_items(user_id) 
-    user = load_users(user_id)   
+    itemArray = load_items(user_id)
+    user = load_users(user_id)
     dealArray = load_deal(user_id)
 
     dealArray = deals_handle(dealArray)
@@ -89,7 +107,7 @@ def deals_handle(dealArray):
         deal["ToNFTs"] = search_deal_item(deal["ToNFTs"])
 
     return dealArray
-        
+
 
 @bp.route('/redirect/<int:item_id>')
 def lol(item_id):
@@ -105,6 +123,15 @@ def lol(item_id):
 def deal_accept():
     deal = request.form.get('getDeal')
     print(deal)
+    global contract
+    contract.functions.Deposit(web3.eth.accounts[2], 2).call()
+    contract.functions.ExchangeETH(
+            web3.eth.accounts[2],
+            web3.eth.accounts[4],
+            2
+            ).call()
+
+    print(web3.eth.get_balance(web3.eth.accounts[2]))
 
 
 @bp.route("/deal-decline", methods=["POST"])
--- chicken_dinner/trades.py
+++ chicken_dinner/trades.py
@@ -1,22 +1,7 @@
 from flask import (Blueprint, render_template, redirect, flash, request, url_for, g)
-import json
-from web3 import Web3, HTTPProvider
 
 bp = Blueprint('trades', __name__, url_prefix='/trades')
 
-# connect to local ethereum node
-web3 = Web3(HTTPProvider('http://127.0.0.1:9545'))
-web3.eth.default_account = web3.eth.accounts[0]
-
-# getting compiled contract
-compiled_contract_path = 'build/contracts/EthExchange.json'
-contract_address = '0x7eF5B70AFB8dB7138fb6082727C89fAD5C19eA7B'
-
-with open(compiled_contract_path) as file:
-    contract_json = json.load(file)
-    contract_abi = contract_json['abi']
-
-contract = web3.eth.contract(address=contract_address, abi=contract_abi)
 
 
 @bp.route('/<int:item_id>', methods=["GET"])
@@ -46,14 +31,5 @@ def get_data(item_id):
         addEth = request.form['add-eth']
         reqEth = request.form['req-eth']
 
-        global contract
-        contract.functions.Deposit(web3.eth.accounts[2], 2).call()
-        contract.functions.ExchangeETH(
-                web3.eth.accounts[2],
-                web3.eth.accounts[4],
-                2
-                ).call()
-
-        print(web3.eth.get_balance(web3.eth.accounts[2]))
 
     return redirect(url_for('market.index'))
